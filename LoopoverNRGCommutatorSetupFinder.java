60c79fc5c1e684c4437563fa0830ecc7bd777907
coolcomputery <52200199+coolcomputery@users.noreply.github.com>
2021-06-11 17:52:22 -0400

    add permutation-solving upper bounds
    stdout:
    N=4
    K=3,diameter=36,BFS time=17
    N=5
    K=3,diameter=64,BFS time=24
    K=4,diameter=56,BFS time=325
    N=6
    K=3,diameter=56,BFS time=37
    K=4,diameter=62,BFS time=1433
    N=7
    K=3,diameter=68,BFS time=71
    K=4,diameter=66,BFS time=3329
    N=8
    K=3,diameter=72,BFS time=94
    K=4,diameter=72,BFS time=10075
    N=9
    K=3,diameter=76,BFS time=317
    K=4,diameter=76,BFS time=28149
    N=10
    K=3,diameter=80,BFS time=538
    K=4,diameter=80,BFS time=90128
      N  S3  S4
      4  36  72
      5  64  56
      6  56  62
      7  68  66
      8  72  72
      9  76  76
     10  80  80
      N      God's number lower bound      God's number upper bound
      4                            22                           256
      5                            42                           628
      6                            69                           958
      7                           104                          1526
      8                           148                          2240
      9                           201                          2972
     10                           262                          3930
    
    Process finished with exit code 0

--- LoopoverNRGCommutatorSetupFinder.java
+++ LoopoverNRGCommutatorSetupFinder.java
@@ -365,7 +365,7 @@ public class LoopoverNRGCommutatorSetupFinder {
                 S3[N-Nlo]=bfs3.diam;
             }
             SetupFinder bfs4=swap22bfs(N);
-            if (bfs4==null) S4[N-Nlo]=-1;
+            if (bfs4==null) S4[N-Nlo]=2*S3[N-Nlo];
             else {
                 //verify(N,bfs4);
                 S4[N-Nlo]=bfs4.diam;
@@ -375,5 +375,27 @@ public class LoopoverNRGCommutatorSetupFinder {
         System.out.printf(form,"N","S3","S4");
         for (int N=Nlo; N<=Nhi; N++)
             System.out.printf(form,N,S3[N-Nlo],S4[N-Nlo]);
+        String form2="%3s%30s%30s%n";
+        System.out.printf(form2,"N","God's number lower bound","God's number upper bound");
+        for (int N=Nlo; N<=Nhi; N++) {
+            int s3=S3[N-Nlo], s4=S4[N-Nlo];
+            int[] Wrow=new int[N*N];
+            Wrow[3]=s3; Wrow[4]=s4;
+            for (int K=5; K<=N*N-1; K++) {
+                Wrow[K]=Wrow[K-2]+Math.min(s3,s4);
+                if (K%4==0 && Wrow[K]<K/4*s4)
+                    throw new RuntimeException();
+            }
+            int worst=0;
+            for (int K=3; K<=N*N-1; K++) worst=Math.max(worst,Wrow[K]);
+            worst+=2*(int)Math.floor(N/2);
+            //System.out.println("N="+N+":"+Arrays.toString(Wrow));
+            System.out.printf(form2,N,(int)Math.ceil((logfactorial(N*N)-Math.log(2))/Math.log(4)),worst);
+        }
+    }
+    private static double logfactorial(int N) {
+        double out=0;
+        for (long i=2; i<=N; i++) out+=Math.log(i);
+        return out;
     }
 }
