89fa6fcb4a32fef407c32c8682617f1cf56b2851
coolcomputery <52200199+coolcomputery@users.noreply.github.com>
2021-07-08 18:40:44 -0400

    add nonstrict setting
    allows gripped piece to be anywhere as long as all pieces that are supposed to be solved are solved

--- LoopoverNRGBFS.java
+++ LoopoverNRGBFS.java
@@ -14,15 +14,13 @@ public class LoopoverNRGBFS {
     private boolean[] Rfree, Cfree;
     private int[] tofree, freeto;
     public int K;
-    private int[] solvedscrm;
-    private int solvedscrmcode;
     //BFS stuff
     private int M;
     private int[][] mvactions, mvs;
     public int ncombos;
     private long[] data;
     public List<int[]> fronts;
-    public int D, Dreturning;
+    public int D;
     //c-->(depth,move from parent combo to c,parent combo)
     private long compressed(int depth, int parent, int move) {
         return ((long)depth*M+move)*ncombos+parent;
@@ -36,9 +34,11 @@ public class LoopoverNRGBFS {
     private int mvi(int code) {
         return data[code]==-1?-1:(int)((data[code]/ncombos)%M);
     }
-    public LoopoverNRGBFS(int R, int C, int gr, int gc, String rf0, String cf0, String rf1, String cf1) {
+    public LoopoverNRGBFS(int R, int C, int gr, int gc, String rf0, String cf0, String rf1, String cf1, boolean strict) {
         //RxC NRG Loopover, gripped piece at (gr,gc) when board is solved
-        System.out.println(R+"x"+C+": ("+rf0+","+cf0+")-->("+rf1+","+cf1+")");
+        //strict: all scrambles must be solved with gripped piece moved to where it should be in the solved board
+        //nonstrict: gripped piece can be anywhere as long as all other pieces that need to be solved are solved
+        System.out.println(R+"x"+C+": ("+rf0+","+cf0+")-->("+rf1+","+cf1+") "+(strict?"strict":"nonstrict"));
         long st=System.currentTimeMillis();
         this.R=R; this.C=C;
         Rfree=mask(rf0); Cfree=mask(cf0);
@@ -66,25 +66,6 @@ public class LoopoverNRGBFS {
                 );
             System.out.println();
         }
-        K=1; //include gripped piece
-        for (int r=0; r<R; r++)
-            for (int c=0; c<C; c++)
-                if ((Rfree[r]||Cfree[c])&&!(Rnfree[r]||Cnfree[c]))
-                    K++;
-        solvedscrm=new int[K];
-        solvedscrm[0]=tofree[gr*C+gc];
-        for (int r=0, idx=1; r<R; r++)
-            for (int c=0; c<C; c++)
-                if ((Rfree[r]||Cfree[c])&&!(Rnfree[r]||Cnfree[c]))
-                    solvedscrm[idx++]=tofree[r*C+c];
-        System.out.println(Arrays.toString(solvedscrm));
-        {
-            long tmp=1;
-            for (int rep=0; rep<K; rep++) tmp*=Nfree-rep;
-            if (tmp>400_000_000) throw new RuntimeException("Too many combinations to handle.");
-            ncombos=(int)tmp;
-        }
-        System.out.println("ncombos="+ncombos);
         M=2*R+2*C;
         mvactions=new int[M][]; mvs=new int[M][]; {
             //mvactions[m][i]=free loc. that i-th free loc. will go to after the m-th move is applied
@@ -112,28 +93,56 @@ public class LoopoverNRGBFS {
                 idx++;
             }
         }
+        K=1; //include gripped piece
+        for (int r=0; r<R; r++)
+            for (int c=0; c<C; c++)
+                if ((Rfree[r]||Cfree[c])&&!(Rnfree[r]||Cnfree[c]))
+                    K++;
+        {
+            long tmp=1;
+            for (int rep=0; rep<K; rep++) tmp*=Nfree-rep;
+            if (tmp>400_000_000) throw new RuntimeException("Too many combinations to handle.");
+            ncombos=(int)tmp;
+            System.out.println("ncombos="+ncombos+", nreturningcombos="+(ncombos/Nfree));
+        }
         //BFS
         data=new long[ncombos]; Arrays.fill(data,-1);
         fronts=new ArrayList<>();
-        solvedscrmcode=comboCode(solvedscrm);
-        fronts.add(new int[] {solvedscrmcode});
-        data[solvedscrmcode]=0;
+        {
+            List<Integer> solvedcodes=new ArrayList<>();
+            for (int grow=0; grow<R; grow++)
+            for (int gclm=0; gclm<C; gclm++)
+            if (strict?(grow==gr&&gclm==gc):(Rnfree[grow]&&Cnfree[gclm])) {
+                int[] solvedscrm=new int[K];
+                solvedscrm[0]=tofree[grow*C+gclm];
+                for (int r=0, idx=1; r<R; r++)
+                    for (int c=0; c<C; c++)
+                        if ((Rfree[r]||Cfree[c])&&!(Rnfree[r]||Cnfree[c]))
+                            solvedscrm[idx++]=tofree[r*C+c];
+                System.out.println(Arrays.toString(solvedscrm));
+                int solvedscrmcode=comboCode(solvedscrm);
+                solvedcodes.add(solvedscrmcode);
+                data[solvedscrmcode]=0;
+            }
+            fronts.add(new int[solvedcodes.size()]);
+            for (int i=0; i<fronts.get(0).length; i++)
+                fronts.get(0)[i]=solvedcodes.get(i);
+        }
         int[] nfront=new int[ncombos];
-        int reached=0, returningreached=0;
-        //a state is "returning" if it keeps the gripped piece at where it is supposed to be
-        //TODO: DISTINGUISH SCRAMBLES BASED ON WHETHER THEY PRESERVE GRIPPED PIECE'S SOLVED LOCATION
-        for (D=0, Dreturning=0;; D++) {
+        int reached=0;
+        for (D=0;; D++) {
             if (fronts.get(D).length==0) break;
             reached+=fronts.get(D).length;
-            int nreturning=0, sz=0;
+            int sz=0;
             for (int f:fronts.get(D)) {
                 int[] scrm=codeCombo(f);
-                if (scrm[0]==solvedscrm[0]) nreturning++;
                 int mr=freeto[scrm[0]]/C, mc=freeto[scrm[0]]%C;
                 int[] mvlist=Rfree[mr]?
                                 (Cfree[mc]?new int[] {mr*2,mr*2+1,2*R+mc*2,2*R+mc*2+1}:new int[] {mr*2,mr*2+1}):
                             Cfree[mc]?new int[] {2*R+mc*2,2*R+mc*2+1}:new int[] {};
-                for (int mi:mvlist) {
+                int invprevmv=D==0?-1:(mvi(f)^1);
+                for (int mi:mvlist)
+                if (mi!=invprevmv) {
                     int nf=comboCode(scrm,mvactions[mi]);
                     if (data[nf]==-1) {
                         data[nf]=compressed(D+1,f,mi);
@@ -141,14 +150,14 @@ public class LoopoverNRGBFS {
                     }
                 }
             }
-            returningreached+=nreturning;
-            System.out.println((D)+":"+fronts.get(D).length+","+nreturning);
-            if (nreturning>0) Dreturning=D+1;
+            System.out.println((D)+":"+fronts.get(D).length);
             fronts.add(new int[sz]);
             System.arraycopy(nfront,0,fronts.get(D+1),0,sz);
         }
-        System.out.println("#reached="+reached+", #returningreached="+returningreached);
-        System.out.println("D="+D+", Dreturning="+Dreturning);
+        System.out.println("#reached="+reached);
+        if (reached!=ncombos)
+            System.out.printf("WARNING: reached=%d!=ncombos=%d%n",reached,ncombos);
+        System.out.println("D="+D);
         System.out.println("total time="+(System.currentTimeMillis()-st));
     }
     /*
@@ -207,11 +216,14 @@ public class LoopoverNRGBFS {
     }
     public List<int[]> solvemvs(int code) {
         List<int[]> out=new ArrayList<>();
-        for (int c=code; c!=solvedscrmcode; c=par(c)) {
+        for (int c=code; depth(c)>0; c=par(c)) {
             int[] mv=mvs[mvi(c)];
             out.add(new int[] {mv[0],mv[1],-mv[2]});
         }
         return out;
+    }
+    public List<int[]> solvemvs(int[] scramble) {
+        return solvemvs(comboCode(scramble));
     }
     public static String mvseqStr(List<int[]> S) {
         StringBuilder str=new StringBuilder();
@@ -220,9 +232,11 @@ public class LoopoverNRGBFS {
         return str.toString();
     }
     public static void main(String[] args) {
-        LoopoverNRGBFS t=new LoopoverNRGBFS(5,5,0,0,"11111","11111","11001","11001");
-        System.out.println(mvseqStr(t.solvemvs(t.comboCode(new int[] {6,1,2,3,4}))));
-        LoopoverNRGBFS t2=new LoopoverNRGBFS(5,5,0,0,"11001","11001","11000","11000");
-        System.out.println(mvseqStr(t2.solvemvs(t2.comboCode(new int[] {0,1,2,3,4,5}))));
+        long st=System.currentTimeMillis();
+        System.out.println(mvseqStr(new LoopoverNRGBFS(5,5,0,0,"11111","11111","11001","11001",true).solvemvs(new int[] {6,1,2,3,4})));
+        System.out.println("time="+(System.currentTimeMillis()-st));
+        st=System.currentTimeMillis();
+        System.out.println(mvseqStr(new LoopoverNRGBFS(5,5,0,0,"11111","11111","11001","11001",false).solvemvs(new int[] {6,1,2,3,4})));
+        System.out.println("time="+(System.currentTimeMillis()-st));
     }
 }
