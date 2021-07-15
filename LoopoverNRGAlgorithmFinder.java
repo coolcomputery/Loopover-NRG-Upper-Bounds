7f9dd469546f1231d9ecaf04ba955b7cbadd6015
coolcomputery <52200199+coolcomputery@users.noreply.github.com>
2021-07-14 23:35:54 -0400

    all 5x5 NRG 2,2-cycles of length <=32
    stdout in previous commit was incorrect as it contained duplicate algorithms (and in fact one of the 32-move 2,2-cycles reduces to a 30-move 2,2-cycle after doing a list rotation on its moves)
    stdout:
    
    L=32
        10.000   117907264           0
        27.182   348605202           0
        41.132   537711833           2
        56.522   745882397           3
        73.890   998138935           6
        93.564  1273014547          10
       115.824  1607499743          18
       140.940  1979957685          19
       169.188  2395209013          25
       200.855  2846635427          27
       236.243  3371715319          33
       275.671  3965193050          39
       319.477  4624396276          54
       368.019  5322803801          56
       421.678  6127196515          59
       480.856  7003595459          64
       545.981  7892059640          69
       617.507  8833225301          85
       695.913  9937226589          96
       781.710 11077981159         115
       875.435 12216097063         136
       977.658 13386286507         143
      1088.984 14556258178         145
      1210.049 16000774331         148
      1341.528 17633530751         154
      1484.131 19384655240         158
      1638.611 21284754737         167
      1805.761 23351330787         194
      1986.416 25511064285         215
      2181.460 27845962540         223
      2391.821 30681308111         266
      2618.480 33687445869         297
      2862.467 36880141296         317
      3124.869 39846348531         321
      3406.828 43110036978         334
    #dfs() calls=44431816427
    #strict blobs with score 4=346
    [DDLDRULURULURDLDDRDLURULURULDR, DDLDLURDRUURDLDLUULDRDRUURULDLUR, DDLDRULURURULDRDLLDRDLURURULURDL, DLDLDRURULULDRDLURDRULDRDLULURUR]
    num=4
    time=3502742
    
    Process finished with exit code 0

--- LoopoverNRGAlgorithmFinder.java
+++ LoopoverNRGAlgorithmFinder.java
@@ -5,6 +5,12 @@ public class LoopoverNRGAlgorithmFinder {
     private static int mod(int n, int k) {
         return (n%k+k)%k;
     }
+    private static final Comparator<String> algcomp=new Comparator<String>() {
+        @Override
+        public int compare(String o1, String o2) {
+            return o1.length()==o2.length()?o1.compareTo(o2):o1.length()-o2.length();
+        }
+    };
     private static int N, K;
     private static long st, mark0=10_000, mark, stage;
     private static long ncalls;
@@ -31,29 +37,26 @@ public class LoopoverNRGAlgorithmFinder {
         if (mincost>K-tlen) //not enough moves available to unshift all rows and columns
             return;
         //System.out.println(tlen);
-        if (tlen==K) {
-            //System.out.println(Arrays.toString(freq));
-            if (dr==0&&dc==0) {
-                boolean good=true;
-                for (int r=0; r<N&&good; r++) if (rshift[r]!=0) good=false;
-                for (int c=0; c<N&&good; c++) if (cshift[c]!=0) good=false;
-                if (good) {
-                    StringBuilder tmp=new StringBuilder();
-                    for (int i=0; i<idx; i++) {
-                        int reps=Math.abs(syllLens[i]);
-                        for (int rep=0; rep<reps; rep++)
-                            tmp.append(i%2==0?(syllLens[i]>0?'R':'L'):(syllLens[i]>0?'D':'U'));
-                    }
-                    String alg=tmp.toString();
-                    int[][] ret=LoopoverNRGSetup.effect(N,alg);
-                    int scr=ret[0].length;
-                    if (results.containsKey(scr)) {
-                        results.get(scr).put(alg,ret);
-                    }
+        if (dr==0&&dc==0) {
+            boolean good=true;
+            for (int r=0; r<N&&good; r++) if (rshift[r]!=0) good=false;
+            for (int c=0; c<N&&good; c++) if (cshift[c]!=0) good=false;
+            if (good) {
+                StringBuilder tmp=new StringBuilder();
+                for (int i=0; i<idx; i++) {
+                    int reps=Math.abs(syllLens[i]);
+                    for (int rep=0; rep<reps; rep++)
+                        tmp.append(i%2==0?(syllLens[i]>0?'R':'L'):(syllLens[i]>0?'D':'U'));
                 }
+                String alg=tmp.toString();
+                int[][] ret=LoopoverNRGSetup.effect(N,alg);
+                int scr=ret[0].length;
+                if (results.containsKey(scr))
+                    results.get(scr).put(alg,ret);
             }
-            return;
         }
+        if (tlen==K)
+            return;
         for (int offset=1; offset<N; offset++) {
             int len=Math.min(offset,N-offset);
             //WLOG first syllable moves gripped piece to the right
@@ -85,41 +88,26 @@ public class LoopoverNRGAlgorithmFinder {
         System.out.println("#dfs() calls="+ncalls);
         System.out.println("#strict blobs with score 4="+results.get(4).size());
     }
-    private static List<String> primaryAlgs(int N, Collection<String> algs) {
-        List<String> primaryAlgs=new ArrayList<>();
+    private static Set<String> primaryAlgs(int N, Collection<String> algs) {
+        TreeSet<String> primaryAlgs=new TreeSet<>(algcomp);
         Set<String> seen=new HashSet<>();
         for (String alg:algs) {
             String red=LoopoverNRGSetup.reduced(N,alg);
             if (!seen.contains(red)) {
-                primaryAlgs.add(red);
+                TreeSet<String> group=new TreeSet<>(algcomp);
                 for (int i=0; i<red.length(); i++) {
                     StringBuilder s=new StringBuilder();
                     //count rotated/reflected/inverted versions of an algorithm to be equivalent
                     for (int k=0; k<red.length(); k++)
                         s.append(red.charAt((k+i)%red.length()));
                     for (int b=0; b<16; b++)
-                        seen.add(LoopoverNRGSetup.reduced(N,LoopoverNRGSetup.transformed(s.toString(),b)));
+                        group.add(LoopoverNRGSetup.reduced(N,LoopoverNRGSetup.transformed(s.toString(),b)));
                 }
+                primaryAlgs.add(group.first());
+                seen.addAll(group);
             }
         }
-        primaryAlgs.sort(new Comparator<String>() {
-            @Override
-            public int compare(String o1, String o2) {
-                return o1.length()==o2.length()?o1.compareTo(o2):o1.length()-o2.length();
-            }
-        });
         return primaryAlgs;
-    }
-    private static String inv(String mvs) {
-        StringBuilder out=new StringBuilder();
-        for (int i=mvs.length()-1; i>-1; i--) {
-            char c=mvs.charAt(i);
-            out.append(c=='D'?'U':c=='R'?'L':c=='U'?'D':'R');
-        }
-        return out.toString();
-    }
-    public static String comm(String A, String B) {
-        return A+B+inv(A)+inv(B);
     }
     public static void main(String[] args) {
         N=5;
@@ -127,7 +115,7 @@ public class LoopoverNRGAlgorithmFinder {
             System.out.println("L="+L);
             st=System.currentTimeMillis();
             blobs(L);
-            List<String> algs=primaryAlgs(N,results.get(4).keySet());
+            Set<String> algs=primaryAlgs(N,results.get(4).keySet());
             System.out.println(algs);
             System.out.println("num="+algs.size());
             System.out.println("time="+(System.currentTimeMillis()-st));
